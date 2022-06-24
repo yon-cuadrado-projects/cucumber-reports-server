abd1681624a581cae64598ef4b59ac583768a0f5
Yon Cuadrado <yon.cuadrado@gmail.com>
2022-06-24 09:45:50 +0200

    updated eslint rules and husky scripts

--- .eslintrc.js
+++ .eslintrc.js
@@ -160,7 +160,7 @@ module.exports = {
         "object-curly-spacing": ["warn", "always"],
         "array-bracket-spacing": ["warn", "always"],
         "max-classes-per-file": ["error",1],
-        "max-len": ["error",{"ignorePattern": "\\s*from|class [a-zA-Z]+ implements|\\s*\\* |\\s*// ","code": 150}],
+        "max-len": ["error",{"ignorePattern": "\\s*from|class [a-zA-Z]+ implements|\\s*\\* |\\s*// ","code": 190}],
         "no-console": ["error",{
                 "allow": [
                     "log",
--- src/resources/scripts/ts/update-report-resources.ts
+++ src/resources/scripts/ts/update-report-resources.ts
@@ -5,25 +5,22 @@ const resourcesData =  '../configuration/resources-data.json';
 const resourcesFolder = path.join( __dirname,'../../../resources/dependencies' );
 const indexEjsFile = path.join( __dirname,'../../../lib/server/views/index.ejs' );
 
-const updateResourcesPropertiesInConfigurationFiles = async (configurationData: Models.ResourceProperties[] ): Promise<boolean> =>{
-  const resourcesModification = configurationData.map( async dependency => {
-    return dependencyModificationFunctions.updateResourcesForOneDependency( dependency, resourcesFolder,[ indexEjsFile ] ); 
-  } );
+const updateResourcesPropertiesInConfigurationFiles = async ( configurationData: Models.ResourceProperties[] ): Promise<boolean> =>{
+  const resourcesModification = configurationData.map( async dependency => dependencyModificationFunctions.updateResourcesForOneDependency( dependency, resourcesFolder,[ indexEjsFile ] ) );
 
-  return ( await Promise.all( resourcesModification ) ).filter( modification => modification === true ).length > 0;
+  return ( await Promise.all( resourcesModification ) ).filter( modification => modification ).length > 0;
 };
 
 const updateResourcesPropertiesConfigurationJson = async ( configurationData: Models.ResourceProperties[] ): Promise<void> =>{
   await CommonFunctions.saveJsonFile<Models.ResourceProperties[]>( path.join( __dirname,'./' ),resourcesData, configurationData );
 };
 
-
 const updateResourcesProperties = async (): Promise<void> =>{
   const configurationData = await CommonFunctions.readJsonFile<Models.ResourceProperties[]>( path.join( __dirname, resourcesData ) );
-  if(configurationData){
-    const isConfigurationUpdated = await updateResourcesPropertiesInConfigurationFiles(configurationData);
-    if(isConfigurationUpdated){
-      await updateResourcesPropertiesConfigurationJson(configurationData)
+  if( configurationData ){
+    const isConfigurationUpdated = await updateResourcesPropertiesInConfigurationFiles( configurationData );
+    if( isConfigurationUpdated ){
+      await updateResourcesPropertiesConfigurationJson( configurationData );
     }
   }
 };
