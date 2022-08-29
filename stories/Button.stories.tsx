5e732bd371c2e255a821c733f15bbe1e66e0a254
Yon Cuadrado <yon.cuadrado@gmail.com>
2022-08-29 21:37:58 +0200

    error fixed

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -3,14 +3,17 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { Button } from './Button';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
-export default {
+
+const button: ComponentMeta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' }
   }
-} as ComponentMeta<typeof Button>;
+};
+
+export default button;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: ComponentStory<typeof Button> = ( args ) => <Button {...args} />;
